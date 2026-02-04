db90c5b046ac683b983cc6dbd8b6b5c5d1f03538
JingweiC <ianchen7007@gmail.com>
2026-02-04 18:41:05 +1100

    cdf

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector(".theme-toggle");
+
+toggleBtn.addEventListener('click', () => {
+  if (document.documentElement.getAttribute('data-theme') === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'light'); 
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+});
